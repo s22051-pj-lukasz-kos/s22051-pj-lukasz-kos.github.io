3b4fc8b330248d72bdcf0427eb4919cdedb1d719
s22051-pj-lukasz-kos <s22051@pjwstk.edu.pl>
2022-01-18 00:30:57 +0100

    Częściowo wykonany CSS

--- scripts/second.js
+++ scripts/second.js
@@ -1,10 +1,14 @@
 $(() => {
-  function hamburger() {
-    let x = $(".myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  // aktywacja aktualnie załadowanej strony w navbarze
+  let url = document.documentURI;
+  $(".navLinks").removeClass("active");
+  if (url.includes("index")) {
+    $("#indexLink").addClass("active");
+  } else if (url.includes("form")) {
+    $("#formLink").addClass("active");
+  } else if (url.includes("measure")) {
+    $("#measureLink").addClass("active");
+  } else if (url.includes("glossary")) {
+    $("#glossLink").addClass("active");
   }
 });
